be244e2eb65a12fb85e2b88bb1c3c8b506e1be8d
JyotiDubey <jyoti@freshdesk.com>
2018-01-29 23:28:58 +0530

    Initial Commit

---
+++ swift_viperTests/swift_viperTests.swift
@@ -0,0 +1,36 @@
+//
+//  swift_viperTests.swift
+//  swift_viperTests
+//
+//  Created by Jyoti Dubey on 29/01/18.
+//  Copyright © 2018 Jyoti Dubey. All rights reserved.
+//
+
+import XCTest
+@testable import swift_viper
+
+class swift_viperTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
